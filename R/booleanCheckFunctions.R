9680d276e63f5eefbf30920c86f7c4242d1234f4
Klaus Herrmann <herk1401@dmat-herrma-01i.DMat.fsci.usherbrooke.ca>
2023-04-15 14:38:14 -0400

    Adds function to check if a variable is a scalar integer value.

--- R/booleanCheckFunctions.R
+++ R/booleanCheckFunctions.R
@@ -13,3 +13,10 @@ is.realNumber <- function(x) is.atomic(x) && length(x) == 1L && !is.character(x)
 #' @export
 is.complexNumber <- function(x) is.atomic(x) && length(x) == 1L && !is.character(x) #https://stackoverflow.com/questions/38088392/how-do-you-check-for-a-scalar-in-r
 
+#' Check if an argument is a scalar integer.
+#'
+#' @param x Value to be tested.
+#' @return Boolean TRUE if it is a integer scalar value, FALSE otherwise
+#' @export
+is.integerNumber <- function(x) is.atomic(x) && length(x) == 1L && !is.character(x) && x%%1 == 0
+
